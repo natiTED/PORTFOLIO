6fe75e85a4a0e8a7f000d8fdb9b053cffbdf90ef
natiTED <123365384+natiTED@users.noreply.github.com>
2024-08-26 20:09:27 +0300

    Update Hero.tsx
    update1

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,7 +31,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            NATNAEL TEWODROS Portfolio
+            WELCOME TO NAATY&apos;S
           </h2>
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
