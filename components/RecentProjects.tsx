d9c27b6dd50fd160849bb93579cbfece02ecd4ac
natiTED <123365384+natiTED@users.noreply.github.com>
2024-08-27 00:26:43 +0300

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -66,7 +66,7 @@ const RecentProjects = () => {
 
                 <div className="flex justify-center items-center">
                   <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Check Live Site
+                    Check Repository
                   </p>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
