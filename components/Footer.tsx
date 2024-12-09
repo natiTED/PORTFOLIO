e621a0b98bd38d330b4cbc259fc3f62069c1a1fd
natiTED <123365384+natiTED@users.noreply.github.com>
2024-12-09 19:47:17 +0300

    Update Footer.tsx 14

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,16 +17,11 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a
-          href=""
-          target="_blank"
-        >
           <MagicButton
             title="want to know more about me? natnaeltewodros9@gmail.com"
             icon={<FaLocationArrow />}
             position="right"
           />
-        </a>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
