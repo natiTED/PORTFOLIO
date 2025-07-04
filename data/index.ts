861f22e7cd9382fd5c634975323a685f0e001613
natiTED <123365384+natiTED@users.noreply.github.com>
2025-07-04 16:32:33 +0300

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -37,7 +37,7 @@ export const gridItems = [
   },
   {
     id: 4,
-    title: "Tech enthusiast with a passion for development.",
+    title: "Tech enthusiast with a passion for development, Cloud Computing, and AI/ML.",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
@@ -49,7 +49,7 @@ export const gridItems = [
   {
     id: 5,
     title:
-      "Currently doing projects by building dynamic websites on AWS Resources",
+      "Currently building a rhobust, scalable, and secure API for a mobile and web application",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
@@ -59,7 +59,7 @@ export const gridItems = [
   },
   {
     id: 6,
-    title: "Do you want to start a project together?",
+    title: "Do you want a chat to know more about me or start a project together?",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
@@ -70,8 +70,30 @@ export const gridItems = [
 ];
 
 export const projects = [
-  {
+    {
     id: 1,
+    title: "HULUBEJE",
+    des: "A modern ecommerce application with multiple features such as cinema ticket booking, Food delivery, hotel booking, and many many more",
+    img: "/hulubeje.jpg",
+    iconLists: [
+      "/aspnet.avif",
+      "/c.png",
+      "/sql.jpg",
+      "/flutter.svg",
+      "/api.webp",
+    ],
+    link: "https://getus.hulubeje.com",
+  },
+  {
+    id: 2,
+    title: "Bingo game Management website",
+    des: "A website for playing bingo games with friends and family, with advanced functionalities like checking sales for each transactions, maintaining credit/debit balance, and admin dashboard for maintaining commission and sales report",
+    img: "/bingo.png",
+    iconLists: ["/aspnet.avif", "/c.png", "/next.svg", "/tail.svg", "/sql.jpg"],
+    link: "https://github.com/natiTED",
+  },
+  {
+    id: 3,
     title: "ECOMMERCE WEBSITE",
     des: " A fully responsive E-commerce website with features such as product listing, shopping cart under different categories, and integrated with PayPal.",
     img: "/ecomm-pic.png",
@@ -82,35 +104,7 @@ export const projects = [
       "/express.png",
       "/mongo.png",
     ],
-    link: "https://github.com/natiTED/MERN-NAATY-STORE",
-  },
-  {
-    id: 2,
-    title: "MOVIES APP",
-    des: "A movie app with different functionalities like searching by category, date, and professional admin dashboard.",
-    img: "/movies-app.png",
-    iconLists: [
-      "/re.svg",
-      "/tail.svg",
-      "/nodejs.png",
-      "/express.png",
-      "/mongo.png",
-    ],
-    link: "https://github.com/natiTED/MERN-NAATY-MOVIES",
-  },
-  {
-    id: 3,
-    title: "BLOG APP",
-    des: "where different tech enthusiasts can upload specific research and new information on emerging technologies for people interested in it.",
-    img: "/blogg.png",
-    iconLists: [
-      "/re.svg",
-      "/tail.svg",
-      "/nodejs.png",
-      "/express.png",
-      "/mongo.png",
-    ],
-    link: "https://github.com/natiTED/MERN-BLOG-APP",
+    link: "https://github.com/natiTED",
   },
   {
     id: 4,
@@ -125,6 +119,21 @@ export const projects = [
       "/mongo.png",
     ],
     link: "https://github.com/natiTED/REMOTELY",
+  },
+
+  {
+    id: 5,
+    title: "NEUROCARE",
+    des: "a website where multiple neurosis conditions can be diagnosed and treated",
+    img: "/neurocare.png",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "/nodejs.png",
+      "/express.png",
+      "/mongo.png",
+    ],
+    link: "https://github.com/natiTED",
   },
 ];
 
@@ -198,16 +207,23 @@ export const workExperience = [
   {
     id: 1,
     title: "Frontend Engineer Intern",
-    desc: "Worked in one of the biggest and pioneer software companies in Ethiopia, CNET SOFTWARE TECHNOLOGIES. I spent 3 months in the projects department,",
+    desc: "Had an intership at one of the biggest and pioneer software companies in Ethiopia, CNET. Spent 3 months as a website developer.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Freelance Website Project",
-    desc: "Designed and developed a website app for a startup business company.",
+    title: "Freelance fullstack website",
+    desc: "Developed a fullstack website for a client using MERN stack, from initial concept to deployment on Cloud VPS.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Backend Developer",
+    desc: "Currently working fulltime as a backend developer at a very big software company in Ethiopia. Specialised in Ecommerce and Fintech industry.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
   },
 ];
 
@@ -215,12 +231,12 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
-    link: "https://github.com/natiTED",
+    link: "https://github.com/natiTED/",
   },
   {
     id: 2,
     img: "/twit.svg",
-    link: "https://x.com/NaatyTed81560",
+    link: "https://x.com/NaatyTed81560/",
   },
   {
     id: 3,
