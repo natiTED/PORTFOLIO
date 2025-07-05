aa34bdb256f28599b0a00a117404bc70d820c784
natiTED <123365384+natiTED@users.noreply.github.com>
2025-07-05 12:50:37 +0300

    Update Footer.tsx 2

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,6 +23,7 @@ const Footer = () => {
             icon={<FaLocationArrow />}
             position="right"
           />
+          </a>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
